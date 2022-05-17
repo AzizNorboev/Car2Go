f13b02cc750b04aed78eb80c6da9cfb9ce6b3223
AzizNorboev <norboev6170@gmail.com>
2022-05-17 17:16:57 +0500

    improved orders detailed page

---
+++ Car2Go/Car2Go.Data.Models/Review.cs
@@ -0,0 +1,27 @@
+﻿using Car2Go.Data.Common.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Car2Go.Data.Models
+{
+    public class Review : BaseDeletableModel<int>
+    {
+        [Required]
+        public int CarId { get; set; }
+
+        public virtual Car Car { get; set; }
+
+        [Required]
+        public string ApplicationUserId { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+
+        [Required]
+        [Range(1, 5)]
+        public int Rating { get; set; }
+
+        [Required]
+        [MinLength(16)]
+        public string Comment { get; set; }
+    }
+}
